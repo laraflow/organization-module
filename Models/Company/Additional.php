ce34504c119123e65d500358999e5c69ff1cc543
Hafijul Islam <hafijul233@gmail.com>
2021-12-05 11:58:57 +0600

    modal created

---
+++ Models/Company/Additional.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Organization\Models\Company;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Additional extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["name","display_name","description","website"];
+    
+    protected static function newFactory()
+    {
+        return \Modules\Organization\Database\Factories\AdditionalFactory::new();
+    }
+}
